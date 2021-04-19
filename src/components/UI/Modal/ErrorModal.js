9bddbc8d672c24cddd0a94064bfd84554b9d2f4a
somratcste <somratcste@gmail.com>
2021-04-19 17:28:44 +0600

    task: integrate react portal

--- src/components/UI/Modal/ErrorModal.js
+++ src/components/UI/Modal/ErrorModal.js
@@ -2,11 +2,16 @@ import React from 'react';
 import Button from '../Button/Button';
 import Card from '../Card/Card';
 import classes from './ErrorModal.module.css';
+import ReactDOM from 'react-dom';
 
 const ErrorModal = (props) => {
-    return (
-        <div>
-            <div className={classes.backdrop} onClick={props.onConfirm} />
+
+    const Backdrop = props => {
+        return <div className={classes.backdrop} onClick={props.onConfirm} />;
+    };
+
+    const ModalOverlay = props => {
+        return (
             <Card className={classes.modal}>
                 <header className={classes.header}>
                     <h2>{props.title}</h2>
@@ -18,7 +23,25 @@ const ErrorModal = (props) => {
                     <Button onClick={props.onConfirm}>Okay</Button>
                 </footer>
             </Card>
-        </div>
+        );
+    };
+
+    return (
+        <>
+            {
+                ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, 
+                document.getElementById('backdrop-root'))
+            }
+            {
+                ReactDOM.createPortal(
+                    <ModalOverlay 
+                        title={props.title}
+                        message={props.message} 
+                        onConfirm={props.onConfirm}
+                    />,
+                    document.getElementById('overlay-root')
+            )}
+        </>
     );
 };
 
