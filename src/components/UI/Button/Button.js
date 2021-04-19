ba346469894ef5a2d7ebbe21e6e7b15b5df589d3
somratcste <somratcste@gmail.com>
2021-04-19 11:33:38 +0600

    task: add button module

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-import styles from './Button.module.css';
+import classes from './Button.module.css';
 
 const Button = props => {
-  return (
-    <button type={props.type} className={styles.button} onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
+    return (
+        <button type={props.type || 'button'} 
+            className={classes.button} 
+            onClick={props.onClick}
+        >
+            {props.children}
+        </button>
+    );
 };
 
 export default Button;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../UI/Button/Button';
 import Card from '../UI/Card/Card';
 import classes from './AddUser.module.css';
 
@@ -16,7 +17,7 @@ const AddUser = props => {
                 <input id="username" type="text" />
                 <label htmlFor="age">Age (Years)</label>
                 <input id="username" type="number" />
-                <button type="submit">Add User</button>
+                <Button type="submit">Add User</Button>
             </form>
         </Card>
     );
