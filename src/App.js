78b338275139d40100db65c88f544d1ea3493242
somratcste <somratcste@gmail.com>
2021-04-19 09:13:18 +0600

    task: validation and error module start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
 import ReactStyle from './components/ReactStyle/ReactStyle';
+import AddUser from './components/Users/AddUser';
 
 const App = () => {
     return (
         <div>
             {/* Practise React Style */}
             {/* <ReactStyle /> */}
+
+            <AddUser />
         </div>
     );
 };
