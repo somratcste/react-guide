69cfba2af59c95edac4c62ede8ce671aa752c56c
somratcste <somratcste@gmail.com>
2021-04-22 18:06:43 +0600

    task: custom context api for whole auth function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import './App.css';
 import MainHeader from './components/MainHeader/MainHeader';
 import Login from './components/Login/Login';
@@ -10,27 +10,7 @@ import UserList from './components/Users/UserList';
 import AuthContext from './components/store/AuthContext';
 
 const App = () => {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    useEffect(() => {
-        const storedUserLoggedIn = localStorage.getItem('isLoggedIn');
-        if (storedUserLoggedIn === '1') {
-            setIsLoggedIn(true);
-        }
-    }, []);
-
-    const loginHandler = (email, password) => {
-        // We should of course check email and password
-        // But it's just a dummy/ demo anyways
-        localStorage.setItem('isLoggedIn', '1');
-        setIsLoggedIn(true);
-    };
-
-    const logoutHandler = () => {
-        localStorage.removeItem('isLoggedIn');
-        setIsLoggedIn(false);
-    };
-
+    const ctx = useContext(AuthContext);
     return (
         <>
             {/* Practise React Style */}
@@ -40,18 +20,12 @@ const App = () => {
             {/* <ReactError /> */}
 
             {/* Practise Handling side effects, reducers and context API  */}
-            <AuthContext.Provider
-                value={{
-                    isLoggedIn: isLoggedIn,
-                    onLogout: logoutHandler
-                }}
-            >
-                <MainHeader />
-                <main>
-                    {!isLoggedIn && <Login onLogin={loginHandler} />}
-                    {isLoggedIn && <Home onLogout={logoutHandler} />}
-                </main>
-            </AuthContext.Provider>
+            
+            <MainHeader />
+            <main>
+                {!ctx.isLoggedIn && <Login />}
+                {ctx.isLoggedIn && <Home />}
+            </main>
         </>
     );
 };
