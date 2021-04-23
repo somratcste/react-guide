9e3dd4f129752e79ea65338c42dd0ec706b9fbc7
somratcste <somratcste@gmail.com>
2021-04-23 16:44:21 +0600

    task: add meal item form component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,36 +1,13 @@
-import React, { useImperativeHandle, useRef } from 'react';
+import React from 'react';
 import classes from './Input.module.css';
 
-const Input = React.forwardRef((props, ref) => {
-    const inputRef = useRef();
-
-    const activate = () => {
-        inputRef.current.focus();
-    };
-
-    useImperativeHandle(ref, () => {
-        return {
-            activate: activate
-        };
-    });
-
+const Input =props => {
     return (
-        <div
-            className={`${classes.control} ${
-                props.isValid === false ? classes.invalid : ''
-                }`}
-        >
-            <label htmlFor={props.id}>{props.label}</label>
-            <input
-                ref={inputRef}
-                type={props.type}
-                id={props.id}
-                value={props.value}
-                onChange={props.onChange}
-                onBlur={props.onBlur}
-            />
+        <div className={classes.input}>
+            <label htmlFor={props.input.id}>{props.label}</label>
+            <input {...props.input}/>
         </div>
     );
-});
+};
 
 export default Input;
