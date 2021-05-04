f7f6f609ab0e196be8d0fb6016b1555ad38e6504
somratcste <somratcste@gmail.com>
2021-05-04 11:27:38 +0200

    task: working with refs and forwardrefs

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -33,6 +33,7 @@ const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map(meal => (
         <MealItem 
             key={meal.id} 
+            id={meal.id}
             meal={meal}
             name={meal.name} 
             price={meal.price} 
