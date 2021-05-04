d1e9b5479fa22357580f6dee13a02d10663fb3dc
somratcste <somratcste@gmail.com>
2021-05-04 10:19:34 +0200

    task: useContext to calculate cart total

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import ReactHook from './components/ReactHook/ReactHook';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
+import CartProvider from './components/store/CartProvider';
 
 const App = () => {
     const [cartIsShown, setCartIsShown] = useState(false);
@@ -25,7 +26,7 @@ const App = () => {
     }
 
     return (
-        <>
+        <CartProvider>
             {/* Practise React Style */}
             {/* <ReactStyle /> */}
 
@@ -41,7 +42,7 @@ const App = () => {
             <main>
                 <Meals />
             </main>
-        </>
+        </CartProvider>
     );
 };
 
