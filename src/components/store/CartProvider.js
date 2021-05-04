217e7c7a3ff933680869465849a332d9a63b3093
somratcste <somratcste@gmail.com>
2021-05-04 10:44:50 +0200

    task: adding a cart reducer

--- src/components/store/CartProvider.js
+++ src/components/store/CartProvider.js
@@ -1,19 +1,45 @@
+import { useReducer } from "react";
 import CartContext from "./CartContext";
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+
+    return defaultCartState;
+};
+
 const CartProvider = props => {
-    const addItemToCartHandler = item => {};
-    const removeItemFromCartHandler = id =>{};
+
+    const [cartState, dispactCartAction] = useReducer(cartReducer, defaultCartState);
+
+    const addItemToCartHandler = (item) => {
+        dispactCartAction({ type: 'ADD', item: item });
+    };
+    const removeItemFromCartHandler = (id) => {
+        dispactCartAction({type: 'REMOVE', id: id});
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
     };
 
     return <CartContext.Provider value={cartContext}>
         {props.children}
-    </CartContext.Provider>
+    </CartContext.Provider>;
 };
 
 export default CartProvider;
