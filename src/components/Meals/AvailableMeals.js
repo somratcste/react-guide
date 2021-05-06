68ff9c6fd71ab99aeafcef9eab2a69cf3dc89b72
somratcste <somratcste@gmail.com>
2021-05-06 17:33:46 +0200

    task: fetch, loading state and error handling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Card from '../UI/Card/Card';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
@@ -30,23 +31,73 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => (
-        <MealItem 
-            key={meal.id} 
+    const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState();
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch(
+                'https://react-http-6b4a6.firebaseio.com/meals.json'
+            );
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            }
+
+            const responseData = await response.json();
+
+            const loadedMeals = [];
+
+            for (const key in responseData) {
+                loadedMeals.push({
+                    id: key,
+                    name: responseData[key].name,
+                    description: responseData[key].description,
+                    price: responseData[key].price,
+                });
+            }
+
+            setMeals(loadedMeals);
+            setIsLoading(false);
+        };
+
+        fetchMeals().catch((error) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        });
+    }, []);
+
+    if (isLoading) {
+        return (
+            <section className={classes.MealsLoading}>
+                <p>Loading...</p>
+            </section>
+        );
+    }
+
+    if (httpError) {
+        return (
+            <section className={classes.MealsError}>
+                <p>{httpError}</p>
+            </section>
+        );
+    }
+
+    const mealsList = meals.map((meal) => (
+        <MealItem
+            key={meal.id}
             id={meal.id}
-            meal={meal}
-            name={meal.name} 
-            price={meal.price} 
-            description={meal.description} 
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
         />
     ));
 
     return (
         <section className={classes.meals}>
             <Card>
-                <ul>
-                    {mealsList}
-                </ul>
+                <ul>{mealsList}</ul>
             </Card>
         </section>
     );
